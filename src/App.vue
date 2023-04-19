983f76869d68ccffce47f026ffef08601bc3b556
simolorevg <simoneviaggi@libero.it>
2023-04-19 16:29:29 +0200

    seacrch funcition work

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,14 @@ export default {
         params.query = this.store.userInput;
       }
       axios.get(this.store.urlMovies, { params }).then((resp) => {
-        this.store.moviesSelected = resp.results;
+        console.log(resp);
+        this.store.moviesSelected = resp.data.results;
       })
-      // axios.get(this.store.urlTv, { params }).then((resp) => {
-      //   this.store.tvSelected = resp.results;
-      // })
+      axios.get(this.store.urlTv, { params }).then((resp) => {
+        this.store.tvSelected = resp.data.results;
+      })
       console.log(this.store.userInput);
+
     },
     handleFilter() {
       this.filterThings();
@@ -38,14 +40,30 @@ export default {
 
 <template>
   <AppHeader @search="handleFilter" />
-  <h2>Films</h2>
-  <div class="row row-cols-3 g-4">
-    <div class="col" v-for="(movie, index) in store.moviesSelected" :key="index">
-      <AppCard :title="movie.original_title" />
+  <div class="my-container">
+    <h2 v-if="this.store.userInput">Films</h2>
+    <h2 v-else>Cerca un film/serieTv</h2>
+    <div class="row row-cols-3 g-4">
+      <div class="col" v-for="(movie, index) in store.moviesSelected" :key="index">
+        <AppCard :title="movie.title" :language="movie.original_language" :realTitle="movie.original_title"
+          :vote="movie.vote_average" />
+      </div>
+    </div>
+    <h2 v-if="this.store.userInput">Serie TV</h2>
+    <div class="row row-cols-3 g-4">
+      <div class="col" v-for="(fiction, index) in store.tvSelected" :key="index">
+        <AppCard :title="fiction.name" :language="fiction.original_language" :realTitle="fiction.original_name"
+          :vote="fiction.vote_average" />
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @use './style/general.scss' as *;
+
+.my-container {
+  width: 80%;
+  margin: 0 auto;
+}
 </style>
--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -2,17 +2,37 @@
 export default {
     name: 'AppCard',
     props: {
-        image: String,
         title: String,
+        language: String,
+        realTitle: String,
+        vote: String
     }
 }
 </script>
 <template>
     <div class="text-center d-flex flex-column">
-        <img :src="image" alt="imagefilm">
-        <p>{{ title }}</p>
+        <div class="my-card-image">
+            Image section
+        </div>
+        <div class="my-text-image">
+            <h3>{{ realTitle }}</h3>
+            <h4>{{ title }}</h4>
+            <p>{{ language }}</p>
+            <h5>{{ vote }}</h5>
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
 @use '../style/general.scss' as *;
+
+.my-card-image {
+    width: 100%;
+    height: 60%;
+    background-color: orange;
+}
+
+.my-text-image {
+    flex-grow: 1;
+    width: 100%;
+}
 </style>
